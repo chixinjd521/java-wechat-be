92bba168168ad42b8ea2fae48e1f69db1ceb63f8
dadiyang <dadiyang@aliyun.com>
2018-07-18 08:23:57 +0800

    添加webqq的接口调用支持

--- src/main/java/com/dadiyang/wx/controllers/BaseController.java
+++ src/main/java/com/dadiyang/wx/controllers/BaseController.java
@@ -1,12 +1,17 @@
 package com.dadiyang.wx.controllers;
 
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import com.dadiyang.wx.dto.ResultBean;
 import com.dadiyang.wx.util.HttpUtils;
+import org.apache.log4j.Logger;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Controller的基类，提供能用的方法，如获取当前用户名，返回结果，获取request和response对象
@@ -18,6 +23,8 @@ public class BaseController {
     private static Logger logger = Logger.getLogger(BaseController.class);
     protected HttpServletRequest request;
     protected HttpServletResponse response;
+    private static final String CONTENT_TYPE_IMG = "image";
+    private static final String CONTENT_TYPE_JSON = "application/json";
 
     @ModelAttribute
     public void setReqAndRes(HttpServletRequest request, HttpServletResponse response) {
@@ -28,6 +35,37 @@ public class BaseController {
     protected String getUserName() {
         Object username = request.getAttribute("loginUser");
         return username == null ? "" : username.toString();
+    }
+
+    /**
+     * 将请求转发到指定服务地址并将返回数据直接 response
+     *
+     * @param server 服务地址
+     */
+    protected void dispatchToServer(String server) {
+        try {
+            Map<String, String[]> pars = request.getParameterMap();
+            Map<String, String> data = new HashMap<>();
+            if (pars.size() > 0) {
+                for (Map.Entry<String, String[]> entry : pars.entrySet()) {
+                    if (entry.getValue() != null && entry.getValue().length > 0) {
+                        data.put(entry.getKey(), entry.getValue()[0]);
+                    }
+                }
+            }
+            String client = getUserName();
+            data.put("client", client);
+            String url = server + request.getRequestURI();
+            Connection.Response rsp = Jsoup.connect(url).data(data).ignoreContentType(true).execute();
+            if (rsp.contentType().startsWith(CONTENT_TYPE_IMG) && rsp.bodyAsBytes() != null) {
+                writeImage(rsp.bodyAsBytes());
+            } else if (CONTENT_TYPE_JSON.equalsIgnoreCase(rsp.contentType())) {
+                writeStringAsJsonObj(rsp.body());
+            }
+        } catch (Exception e) {
+            logger.error("异常:" + request.getRequestURL(), e);
+            writeJsonObj(ResultBean.createFailResult("发生异常"));
+        }
     }
 
     protected void writeJsonObj(Object obj) {
--- src/main/java/com/dadiyang/wx/controllers/OpenqqController.java
+++ src/main/java/com/dadiyang/wx/controllers/OpenqqController.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * 处理微信相关的请求
- * 对openwx接口的封装，以实现一些管理功能
+ * 处理 QQ 相关的请求
+ * 对 openqq 接口的封装，以实现一些管理功能
  *
  * @author dadiyang
- * @date 2018/3/24
+ * @date 2018/7/16
  */
 @Controller
-@RequestMapping("/openwx/**")
-public class OpenWxController extends BaseController {
-    private static final Logger logger = Logger.getLogger(OpenWxController.class);
+@RequestMapping("/openqq/**")
+public class OpenqqController extends BaseController {
+    private static final Logger logger = Logger.getLogger(OpenqqController.class);
 
-    @Value("${openwxServer}")
+    @Value("${openqqServer}")
     private String server;
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.GET})
